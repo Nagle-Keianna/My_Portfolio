0b3a2971061b860aff1e4c96f75d194243e2af86
Keianna <nagle.keianna@gmail.com>
2021-02-25 23:52:10 +0000

    Add in facts and quotes

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I might have a slight addiction to video games', 'I love Batman and the Joker', 'I turned myself into a pickle Morty!', "I'm pickle Rick!"];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
