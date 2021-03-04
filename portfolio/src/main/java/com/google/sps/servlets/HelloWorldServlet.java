d0eebe3975d551d91dddd1ecded514b72a322dac
Keianna <nagle.keianna@gmail.com>
2021-03-04 23:38:59 +0000

    Changed the hard-coded string

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -18,6 +18,6 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello World!</h1>");
   }
 }
