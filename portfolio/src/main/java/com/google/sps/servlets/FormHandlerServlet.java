9d9ec4786e66703416361bc23bf56fe77af9f3bd
Keianna <nagle.keianna@gmail.com>
2021-03-12 23:17:51 +0000

    Format form servlet to ask for favorite game

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,12 +13,15 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String favoriteGame = request.getParameter("favoriteGame");
+    String name = request.getParameter("name");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println( name + "'s favorite game is " + favoriteGame);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You said your favorite game to play is: " + textValue);
+    response.getWriter().println("You said your favorite game to play is: " + favoriteGame);
+
+    response.sendRedirect("http://knagle-sps-spring21.appspot.com/");
   }
 }
