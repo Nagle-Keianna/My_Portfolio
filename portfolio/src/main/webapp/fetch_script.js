fce4da290c61e8539df68fb8a99e16b5802a22e6
Keianna <nagle.keianna@gmail.com>
2021-03-14 22:29:59 +0000

    Move translation function to JS

--- portfolio/src/main/webapp/fetch_script.js
+++ portfolio/src/main/webapp/fetch_script.js
@@ -8,3 +8,22 @@ async function showVideoGame() {
   factContainer.innerText = textFromResponse[number];
 }
 
+async function requestTranslation() {
+        const text = document.getElementById('text').value;
+        const languageCode = document.getElementById('language').value;
+
+        const resultContainer = document.getElementById('fact-container');
+        resultContainer.innerText = 'Loading...';
+
+        const params = new URLSearchParams();
+        params.append('text', text);
+        params.append('languageCode', languageCode);
+
+        fetch('/arabic', {
+          method: 'POST',
+          body: params
+        }).then(response => response.text())
+        .then((translatedMessage) => {
+          resultContainer.innerText = translatedMessage;
+        });
+      }
